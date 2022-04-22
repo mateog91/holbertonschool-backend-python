bfc2da260b56df97d9e344d0a9b00bdcc37b7fb3
Mateo <mateog91@gmail.com>
2022-04-22 12:46:27 -0500

    UPD: docstrings

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -5,4 +5,14 @@
 
 
 def add(a: float, b: float) -> float:
+    """
+    add: Adds two floats
+
+    Args:
+        a (float): first input
+        b (float): second input
+
+    Returns:
+        float: sum of a and b
+    """
     return a + b
