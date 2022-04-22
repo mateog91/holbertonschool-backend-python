c4c73de550c732d5ab7ae00fb8ea8be93c48b0e6
Mateo <mateog91@gmail.com>
2022-04-22 14:46:33 -0500

    ADD: FIles for task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+    Module for task 3
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    to_str casts a float to a string
+
+    Args:
+        n (float): Input float number
+
+    Returns:
+        str: String representation of the number
+    """
+    return str(n)
