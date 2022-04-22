89affb8ccfa295894373b230dc146916f2c8a0c1
Mateo <mateog91@gmail.com>
2022-04-22 12:52:09 -0500

    ADD: files for task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+    Module for task 1
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    concat concatenates two strings
+
+    Args:
+        str1 (str): First string
+        str2 (str): Second string
+
+    Returns:
+        str: concatenated string
+    """
+    return str1 + str2
