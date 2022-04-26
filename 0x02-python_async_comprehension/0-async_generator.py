cc7fae7d060ff6314f142527cd1ebedc1f912f15
Mateo <mateog91@gmail.com>
2022-04-26 14:19:57 -0500

    changed hint type

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 """Module for Task 0"""
 
-from typing import Iterator
+from typing import Generator
 import random
 import asyncio
 
 
-async def async_generator() -> Iterator[float]:
+async def async_generator() -> Generator[float, None, None]:
     """
     async_generator The coroutine will loop 10 times, each time asynchronously
     wait 1 second
 
     Yields:
-        Iterator[int]: yield a random number between 0 and 10.
+        Generator[float, None, None]: yield a random number between 0 and 10.
     """
     for i in range(10):
         rand_int = random.uniform(0, 10)
