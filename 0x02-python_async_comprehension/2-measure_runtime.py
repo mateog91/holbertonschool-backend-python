7039fb6c7d29bf515f254700545e86f36e17314f
Mateo <mateog91@gmail.com>
2022-04-26 18:40:52 -0500

    ADD: file for task 2

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Module for Task 0"""
+
+import asyncio
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+
+    start = time.perf_counter()
+
+    wait = await asyncio.gather(
+        *(async_comprehension() for _ in range(4))
+    )
+    # wait = await asyncio.gather(async_comprehension(),
+    #                       async_comprehension(),
+    #                       async_comprehension(),
+    #                       async_comprehension())
+    total = time.perf_counter() - start
+    return total
