91810db201c10c7551fbf624e6d3df30a8ea1fc0
Mateo <mateog91@gmail.com>
2022-04-26 12:10:33 -0500

    ADD: new dict, readme file and task 0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Module for Task 0"""
+
+from typing import Iterator
+import random
+import asyncio
+
+
+async def async_generator() -> Iterator[int]:
+    """
+    async_generator The coroutine will loop 10 times, each time asynchronously
+    wait 1 second
+
+    Yields:
+        Iterator[int]: yield a random number between 0 and 10.
+    """
+    for i in range(10):
+        rand_int = random.randint(0, 10)
+        yield rand_int
+        await asyncio.sleep(1)
