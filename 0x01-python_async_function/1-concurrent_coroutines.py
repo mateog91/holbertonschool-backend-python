2455344063fae28eb4a7251532c0098629915325
Mateo <mateog91@gmail.com>
2022-04-25 21:07:19 -0500

    PYCODE STYLE AND DOCSTRING AND TYPED

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
-"""Module for task 0"""
+"""Module for task 1"""
 
 import asyncio
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    wait_n wait n number of times
+
+    Args:
+        n (int): number of times to repeat
+        max_delay (int): max time to wait
+
+    Returns:
+        list[float]: _description_
+    """
     wait_list = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
     wait_list.sort()
     return wait_list
